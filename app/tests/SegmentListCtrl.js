eec73899e93640ab19143ac6ade41bb2b8b6118d
bionara <deathbycornishpastie@hotmail.com>
2014-05-16 16:56:48 +0100

    Initial setup and work on medlar with angularjs

---
+++ app/tests/SegmentListCtrl.js
@@ -0,0 +1,9 @@
+describe('SegmentListCtrl', function(){
+
+  it('should create "phones" model with 3 phones', function() {
+    var scope = {},
+        ctrl = new PhoneListCtrl(scope);
+    expect(scope.phones.length).toBe(3);
+  });
+
+});
